0917fe08f6a9171d1438695207134ff0b0642b99
LuminaHarmony <158002251+LuminaHarmony@users.noreply.github.com>
2024-03-12 16:18:58 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'f67c70e6-cb57-46f6-bf42-b5f2cac32521';
 
 const proxyIPs = ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'workers.bestip.one'];
 
